e3440ef25a21bbbe6aebce849ba86a4cd60ad250
MotyaTheWise <motyathewise@gmail.com>
2017-04-03 15:07:49 +0300

    Add files via upload

---
+++ Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 8;
+    Vector3 targetPos;
+    GameObject ballObj;
+
+	void Start () {
+        
+	}
+	
+	
+	void Update () {
+        ballObj = GameObject.FindGameObjectWithTag("Ball");
+        targetPos = Vector3.Lerp(gameObject.transform.position, ballObj.transform.position, Time.deltaTime * speed);
+        gameObject.transform.position = new Vector3(20, targetPos.y,0);
+	}
+}
